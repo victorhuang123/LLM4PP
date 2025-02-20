54b316b4135c02ade6a8da0bda206b8b1102b774
Xu Zhang <takanashi_rikka@126.com>
2025-02-20 10:06:59 +0800

    [FixBug]Fix bug of run.sh

--- problem1/run.sh
+++ problem1/run.sh
@@ -1,19 +1,19 @@
 #base
 echo "base"
-python evaluation.py --models gpt-4o-mini > base_out
+python generate.py --models gpt-4o-mini > base_out
 
 #s-p norag
 echo "s-p norag"
-python evaluation.py --models gpt-4o-mini --two-stage true --serial-rag false --parallel-rag false --check false > norag_out
+python generate.py --models gpt-4o-mini --two-stage true --serial-rag false --parallel-rag false --check false > norag_out
 
 #s rag then p
 echo "s rag then p"
-python evaluation.py --models gpt-4o-mini --two-stage true --serial-rag true --parallel-rag false --check false > s_rag_p_norag
+python generate.py --models gpt-4o-mini --two-stage true --serial-rag true --parallel-rag false --check false > s_rag_p_norag
 
 #s rag p rag
 echo "s rag p rag"
-python evaluation.py --models gpt-4o-mini --two-stage true --serial-rag true --parallel-rag true --check false > s_rag_p_rag
+python generate.py --models gpt-4o-mini --two-stage true --serial-rag true --parallel-rag true --check false > s_rag_p_rag
 
 #check
 echo "add check"
-python evaluation.py --models gpt-4o-mini --two-stage true --serial-rag true --parallel-rag true --check true > check
+python generate.py --models gpt-4o-mini --two-stage true --serial-rag true --parallel-rag true --check true > check
