df0e276f991630bdf59c5a7df09aec6d08e24d20
Xu Zhang <takanashi_rikka@126.com>
2025-02-21 14:11:27 +0800

    Fix bug of .sh

--- problem1/generate_openai.sh
+++ problem1/generate_openai.sh
@@ -2,25 +2,25 @@ models=("gpt-4o-mini" "gpt-4o")
 
 for model in "${models[@]}"; do
     #--------------------------------------------------#
-    echo ("base")
+    echo "base"
     python generate.py --models $model
     python generate.py --models $model --check true
     #--------------------------------------------------#
 
     #--------------------------------------------------#
-    echo ("s-p norag")
+    echo "s-p norag"
     python generate.py --models $model --two-stage true --serial-rag false --parallel-rag false
     python generate.py --models $model --two-stage true --serial-rag false --parallel-rag false --check true
     #--------------------------------------------------#
     
     #--------------------------------------------------#
-    echo ("s rag then p")
+    echo "s rag then p"
     python generate.py --models $model --two-stage true --serial-rag true --parallel-rag false
     python generate.py --models $model --two-stage true --serial-rag true --parallel-rag false --check true 
     #--------------------------------------------------#
     
     #--------------------------------------------------#
-    echo ("s rag p rag")
+    echo "s rag p rag"
     python generate.py --models $model --two-stage true --serial-rag true --parallel-rag true
     python generate.py --models $model --two-stage true --serial-rag true --parallel-rag true --check true
     #--------------------------------------------------#
